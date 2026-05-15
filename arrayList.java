187e2d0b86af2bb51e342bb0a8309929f717fcbb
GitHubExodus <165914518+GitHubExodus@users.noreply.github.com>
2026-05-16 00:02:15 +0300

    Rename Main class to arrayList

--- arrayList.java
+++ arrayList.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Main {
+public class arrayList {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
